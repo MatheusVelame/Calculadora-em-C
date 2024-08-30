123585991d6f58400190a3e663be2a8bb112484f
velame <mvp2@cesar.school>
2024-08-30 17:53:51 -0300

    finalizando o passo a passo da questão 1/2

--- mvp_q1.c
+++ mvp_q1.c
@@ -91,7 +91,7 @@ void decimal_base2(int numero_decimal, int bit_sinal){
 void decimal_base8(int numero_decimal){
     int converter_numero[100], cont=0, guardar_numero = numero_decimal;
     printf("__________________________\n");
-    printf("\nPasso a passo para tranformar o numero %d em binario:\n\n", numero_decimal);
+    printf("\nPasso a passo para tranformar o numero %d em octal:\n\n", numero_decimal);
     if (numero_decimal < 0){
         printf("Como o numero %d eh menor que 0, o sinal sera -\n\n", numero_decimal);
         numero_decimal*=-1;
@@ -106,7 +106,7 @@ void decimal_base8(int numero_decimal){
             break;
         }
         cont++;
-        guardar_numero = guardar_numero/2;
+        guardar_numero = guardar_numero/8;
     }
 
     if (numero_decimal < 0){
@@ -123,20 +123,46 @@ void decimal_base8(int numero_decimal){
 
 
 void decimal_base16(int numero_decimal){
-    int converter_numero[100], cont = 0;
-    printf("\n\nNumero hexadecimal: 0x");
+    int converter_numero[100], cont = 0, guardar_numero = numero_decimal;
+    printf("__________________________\n");
+    printf("\nPasso a passo para tranformar o numero %d em hexadecimal:\n\n", numero_decimal);
     if (numero_decimal < 0){
-        printf("-");
-        numero_decimal*=-1;
+        printf("Como o numero %d eh menor que 0, o sinal sera -\n\n", numero_decimal);
+        numero_decimal *= -1;
+        guardar_numero *= -1;
     }
     while(1){
         converter_numero[cont] = numero_decimal%16;
         numero_decimal = numero_decimal/16;
+        printf("%d Dividido por 2: %d\n", guardar_numero, numero_decimal);
+        printf("Resto: %d\n\n", converter_numero[cont]);
+        if (converter_numero[cont] == 10){
+            printf("O numero %d na tabela de hexadecimal fica: A\n\n", converter_numero[cont]);
+        }else if (converter_numero[cont] == 11){
+            printf("O numero %d na tabela de hexadecimal fica: B\n\n", converter_numero[cont]);
+        }else if (converter_numero[cont] == 12){
+            printf("O numero %d na tabela de hexadecimal fica: C\n\n", converter_numero[cont]);
+        }else if (converter_numero[cont] == 13){
+            printf("O numero %d na tabela de hexadecimal fica: D\n\n", converter_numero[cont]);
+        }else if (converter_numero[cont] == 14){
+            printf("O numero %d na tabela de hexadecimal fica: E\n\n", converter_numero[cont]);
+        }else if (converter_numero[cont] == 15){
+            printf("O numero %d na tabela de hexadecimal fica: F\n\n", converter_numero[cont]);
+        }
         if (numero_decimal == 0){
             break;
         }
         cont++;
+        guardar_numero = guardar_numero/16;
     }
+
+
+    if (numero_decimal < 0){
+        printf("Resultado: 0x-");
+    }else{
+        printf("Resultado: 0x");
+    }
+
     for (int t=cont;t>=0;t--){
         if (converter_numero[t] == 10){
             printf("A");
@@ -160,8 +186,18 @@ void decimal_base16(int numero_decimal){
 
 void decimal_bcd(int numero_decimal){
     char converter_bcd[1000];
-     printf("\n\nNumero BCD: ");
+
+    printf("__________________________\n");
+    printf("\nPrintando o numero decimal %d, seguindo os seus respectivos numeros binarios (BCD):\n\n", numero_decimal);
+
+    if (numero_decimal < 0){
+        printf("\n\nResultado: -");
+    }else{
+        printf("\n\nResultado: ");
+    }
+
     sprintf(converter_bcd, "%d", numero_decimal);
+
     for (int t=0;converter_bcd[t];t++){
         if (converter_bcd[t] == '0'){
             printf("0000 ");
@@ -189,20 +225,30 @@ void decimal_bcd(int numero_decimal){
 
 
 void decimal_complementoa2(int numero_decimal, int bit_sinal){
-    int decimal_auxiliar, converter_numero[100], cont = 0, cont_aux = 0, converter_numero_aux[100];;
+    int decimal_auxiliar, converter_numero[100], cont = 0, cont_aux = 0, converter_numero_aux[100], guardar_numero = numero_decimal;
     decimal_auxiliar = numero_decimal;
-    printf("\n\nComplemento A2: ");
+    printf("__________________________\n");
+    printf("\nTransformando o numero decimal %d em complemento a2:\n\n", numero_decimal);
     if (numero_decimal >= 0){
         bit_sinal = 0;
+        printf("Como o numero %d eh maior que 0, o bit de sinal sera %d\n\n", numero_decimal, bit_sinal);
     } else{
         bit_sinal = 1;
-        numero_decimal*=-1;
-        decimal_auxiliar*=-1;
+        numero_decimal *= -1;
+        decimal_auxiliar *= -1;
+        guardar_numero *= -1;
+        printf("Como o numero %d eh menor que 0, o bit de sinal sera %d\n\n", numero_decimal, bit_sinal);
     }
+
+    printf("Convertendo para o numero para base dois com 15 bits:\n");
+
     while(cont<=14){
         converter_numero[cont] = numero_decimal%2;
         numero_decimal = numero_decimal/2;
+        printf("%d Dividido por 2: %d\n", guardar_numero, numero_decimal);
+        printf("Resto: %d\n\n", converter_numero[cont]);
         cont++;
+        guardar_numero++;
         }
     if (decimal_auxiliar>0){
         while(1){
@@ -213,9 +259,11 @@ void decimal_complementoa2(int numero_decimal, int bit_sinal){
         }
         cont_aux++;
     }
+
     }
     if (bit_sinal==1){
-        printf("%d ", bit_sinal);
+        printf("Como o bit eh 1 (numero negativo), precisamos inverter os bits ate o primeiro 1:\n");
+        printf("Resultado: %d ", bit_sinal);
         for (int t=14;t>=0;t--){
             if (converter_numero[t]==0 && cont_aux+1<=t){
                 converter_numero[t]=1;
@@ -226,7 +274,8 @@ void decimal_complementoa2(int numero_decimal, int bit_sinal){
             converter_numero[t] = 0;
         }
     } else{
-        printf("%d ", bit_sinal);
+        printf("Como o bit eh 0 (numero positivo), nao precisamos inverter os bits ate o primeiro 1:\n");
+        printf("Resultado: %d ", bit_sinal);
         for (int t=14;t>=0;t--){
             printf("%d", converter_numero[t]);
             converter_numero[t] = 0;
